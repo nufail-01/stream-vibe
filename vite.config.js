f34fcc673b7d5422a8ff3e36a6d17579f17b81b9
Nufail Shaikh <nufailshaikh786@gmail.com>
2026-06-04 03:46:17 +0530

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
+  base: "/stream-vibe/",
 });
